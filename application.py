ee7e3dd19b31a51a4d1b8bf9a79eb7de66f75b11
J.T. Robinson <jaittarius@hotmail.com>
2017-08-08 18:46:06 -0400

    Initial templates added
    Initial HTML templates for application output

--- application.py
+++ application.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from sqlalchemy import create_engine
+from flask import (Flask, render_template, request, redirect, jsonify, url_for,
+                   flash)
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Part, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///computerhw.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
 @app.route('/catalog')
-def items():
-    return "Item Catalog Application"
+def showCategories():
+    categories = session.query(Category).order_by(asc(Category.name))
+    return render_template('publicmain.html', categories=categories)
 
 
 if __name__ == '__main__':
